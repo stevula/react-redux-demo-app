0b0ef0c0158f1180333facf6058520417be7f210
stevula <brodericksteven@gmail.com>
2017-11-29 15:05:59 -0800

    Rename component

--- src/components/SiteHeader/index.js
+++ src/components/SiteHeader/index.js
@@ -3,7 +3,7 @@ import { Navbar } from 'react-materialize'
 import './styles.css';
 import reduxLogo from '../../redux-logo.png';
 
-const Nav = () => {
+const SiteHeader = () => {
   // NOTE: logo height should be equal to materialize-css $navbar-height
   // https://github.com/Dogfalo/materialize/blob/f66e68e8455464c77c252a8149e872d86056ca04/sass/components/_variables.scss#L238
   // there is a slight difference on mobile but it still looks good at 64
@@ -11,4 +11,4 @@ const Nav = () => {
   return <Navbar className='teal lighten-4' brand={logo} />;
 };
 
-export default Nav;
+export default SiteHeader;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { Container } from 'react-materialize'
 import './index.css';
-import Nav from './components/Nav';
+import SiteHeader from './components/SiteHeader';
 import VisibleContributors from './containers/VisibleContributors';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers';
@@ -15,7 +15,7 @@ const store = createStore(reducers, { contributors });
 ReactDOM.render(
   <Provider store={store} >
     <Container>
-      <Nav />
+      <SiteHeader />
       <VisibleContributors />
     </Container>
   </Provider>,
