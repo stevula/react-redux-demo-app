30c1cf72212f1a1d40a7a3c900c19c81d73de025
stevula <brodericksteven@gmail.com>
2017-11-29 14:13:30 -0800

    Add Nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Navbar, NavItem } from 'react-materialize'
+import './styles.css';
+import reduxLogo from '../../redux-logo.png';
+
+const Nav = () => <Navbar brand='Redux Contributors' />;
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { Container } from 'react-materialize'
 import './index.css';
+import Nav from './components/Nav';
 import VisibleContributors from './containers/VisibleContributors';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers';
@@ -12,7 +14,10 @@ const store = createStore(reducers, { contributors });
 
 ReactDOM.render(
   <Provider store={store} >
-    <VisibleContributors />
+    <Container>
+      <Nav />
+      <VisibleContributors />
+    </Container>
   </Provider>,
   document.getElementById('root')
 );
